ff973a595dda80f7dffd3cc96ae37dd0b94b5cbd
Seulji-jo <joseuling@gmail.com>
2024-01-22 04:47:01 +0900

    style: style 수정

--- src/pages/ClockPage.tsx
+++ src/pages/ClockPage.tsx
@@ -2,10 +2,12 @@ import AnalogClock from "../components/AnalogClock";
 
 function ClockPage() {
   return (
-    <div className="inline-flex flex-col gap-10 items-center justify-center">
-      <h1 className="text-6xl text-center font-bold">Analog Clock</h1>
-      <AnalogClock />
-    </div>
+    <main className="w-screen h-screen flex">
+      <div className="inline-flex flex-col gap-20 items-center">
+        <h1 className="text-6xl text-center font-bold">Analog Clock</h1>
+        <AnalogClock />
+      </div>
+    </main>
   );
 }
 
