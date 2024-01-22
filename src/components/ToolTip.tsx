efc6dbea8a70b953ff1cb574c9410fa30ba33145
Seulji-jo <joseuling@gmail.com>
2024-01-23 02:31:23 +0900

    feat: create tooltip - 마우스 우상단 위치 - 현재 시간 표시 - 포지션 업데이트

---
+++ src/components/ToolTip.tsx
@@ -0,0 +1,59 @@
+import { MouseEvent, ReactNode, useEffect, useRef, useState } from "react";
+import { useTimeStore } from "../store/useTimeStore";
+import Portal from "./Portal";
+
+interface ToolTipProps {
+  children: ReactNode;
+}
+
+function Tooltip({ children }: ToolTipProps) {
+  const { time } = useTimeStore();
+  const { hours, minutes, seconds } = time;
+  const [isVisible, setIsVisible] = useState(false);
+  const [position, setPoisiton] = useState({
+    top: 0,
+    left: 0,
+  });
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+
+  const showTip = () => {
+    setIsVisible(true);
+  };
+  const hideTip = () => {
+    setIsVisible(false);
+  };
+
+  const handleToolTipPosition = (e: MouseEvent<HTMLSpanElement>) => {
+    setPoisiton({ top: e.clientY, left: e.clientX });
+  };
+
+  useEffect(() => {
+    if (tooltipRef.current) {
+      tooltipRef.current.style.top = `${position.top - 30}px`;
+      tooltipRef.current.style.left = `${position.left + 10}px`;
+    }
+  }, [position]);
+
+  return (
+    <span
+      onMouseEnter={showTip}
+      onMouseMove={handleToolTipPosition}
+      onMouseLeave={hideTip}
+      className="w-fit h-auto overflow-hidden rounded-[50%]"
+    >
+      {children}
+      <Portal>
+        {isVisible && (
+          <div
+            ref={tooltipRef}
+            className={`min-w-[100px] absolute px-[16px] py-[8px] bg-black bg-opacity-70 rounded-[8px] text-white text-xs font-semibold`}
+          >
+            {hours}:{minutes}:{seconds}
+          </div>
+        )}
+      </Portal>
+    </span>
+  );
+}
+
+export default Tooltip;
--- src/pages/ClockPage.tsx
+++ src/pages/ClockPage.tsx
@@ -1,11 +1,14 @@
 import AnalogClock from "../components/AnalogClock";
+import Tooltip from "../components/ToolTip";
 
 function ClockPage() {
   return (
     <main className="w-screen h-screen flex justify-center">
       <div className="inline-flex flex-col gap-20 items-center">
         <h1 className="text-6xl text-center font-bold">Analog Clock</h1>
-        <AnalogClock />
+        <Tooltip>
+          <AnalogClock />
+        </Tooltip>
       </div>
     </main>
   );
